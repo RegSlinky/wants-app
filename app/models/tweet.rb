0233ec5753d8145a1fcdeb63da4605b517abbe7a
RegSlinky <thesmile0701@gmail.com>
2022-11-16 19:27:02 +0900

    投稿機能の実装

---
+++ app/models/tweet.rb
@@ -0,0 +1,10 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  has_one :item
+  has_many :comments, dependent: :destroy
+
+  validates :artist, presence: true
+  validates :text, {lengh: {maximum: 140}}
+  validates :image, presence: true
+end
