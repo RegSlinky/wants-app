ca11815d6df57ed56400a644af5427d90591e1bb
RegSlinky <thesmile0701@gmail.com>
2022-11-16 13:59:45 +0900

    ユーザー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+       
+         with_options presence: true do
+           validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "を全角で入力してください"}
+           validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "を全角で入力してください"}
+           validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "を全角カタカナで入力してください"}
+           validates :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "を全角カタカナで入力してください"}
+         end
+       
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: "を英字と数字の両方を含めて入力してください"
+         
+         has_many :tweets
+         has_many :comments
+         has_many :items
+         has_many :orders
 end
