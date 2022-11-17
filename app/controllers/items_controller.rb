66a7bb1229d4581047ac26df2e7b36b9fda40e6d
RegSlinky <thesmile0701@gmail.com>
2022-11-17 10:47:34 +0900

    コントローラーの設定

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,26 @@
+class ItemsController < ApplicationController
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:artist, :image, :title, :shipping_fee_status_id, :prefecture_id, :parter_nickname, :price).merge(user_id:current_user.id)
+  end
+
+end
